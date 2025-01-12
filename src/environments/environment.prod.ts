c0788916ea0dde0627f25da54d24153abef7e7ae
Shaham Ali <shahamrana@hotmail.com>
2025-01-12 18:08:52 -0500

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: ''  // Production backend URL
+    API_URL: 'https://payments-app-exam-967adb1a0a11.herokuapp.com/'  // Production backend URL
   };
