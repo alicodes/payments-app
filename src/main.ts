09804480a68489de00c0be7105fa7721a8ee031d
Shaham Ali <shahamrana@hotmail.com>
2025-01-12 12:02:39 -0500

    Removed Animations

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { importProvidersFrom } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
 
@@ -11,7 +10,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes), 
-    provideAnimationsAsync(),
     importProvidersFrom(MatNativeDateModule)
   ]
 }).catch(err => console.error(err));
