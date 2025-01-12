c72bebc6abec0644c858470fe10c0f288ec67338
Shaham Ali <shahamrana@hotmail.com>
2025-01-12 13:52:43 -0500

    Field Mismatch

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { importProvidersFrom } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
 
@@ -10,6 +11,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes), 
+    provideAnimationsAsync(),
     importProvidersFrom(MatNativeDateModule)
   ]
 }).catch(err => console.error(err));
