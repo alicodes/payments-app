f6df54b947ff195e0e3d8e9c0091a2822c2b4ac9
Shaham Ali <shahamrana@hotmail.com>
2025-01-13 01:05:51 -0500

    Environment Change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'https://payments-app-exam-967adb1a0a11.herokuapp.com/'  // Production backend URL
+    API_URL: 'https://payments-api-187e6604d2a7.herokuapp.com/'  // Production backend URL
   };
